50e5d3f3ffde4632ec5a1ed605e56b87a72d520a
Darshan Hulamani <123568236+Darshan-Hulamani@users.noreply.github.com>
2026-06-01 22:17:54 +0530

    Protected Firebase Credentials

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7wfKsaVUnYzoHyLSaxnktJLZQH2sKqu8",
-  authDomain: "saferoute-quantum-quads.firebaseapp.com",
-  projectId: "saferoute-quantum-quads",
-  storageBucket: "saferoute-quantum-quads.firebasestorage.app",
-  messagingSenderId: "424661347697",
-  appId: "1:424661347697:web:221f9feeda36fb60f01ad0",
-  measurementId: "G-ETHW4Y6X4S"
+  apiKey: YOUR_API_KEY,
+  authDomain: YOUR_DOMAIN_AUTHORIZATION,
+  projectId: YOUR_PROJECT_ID,
+  storageBucket: YOUR_STORAGE_BUCKET_LINK,
+  messagingSenderId: YOUR_MESSAGE_SENDER_ID,
+  appId: YOUR_APP_ID,
+  measurementId: YOUR_MEASUREMENTS_ID
 };
 
 const app = initializeApp(firebaseConfig);
